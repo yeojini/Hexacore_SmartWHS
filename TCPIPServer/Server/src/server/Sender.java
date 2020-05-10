51d56c1e824681b80635d55dfa7e0de8bba64d58
Yeojin Choi <who3637@naver.com>
2020-05-11 01:38:29 +0900

    Update Sender.java

--- TCPIPServer/Server/src/server/Sender.java
+++ TCPIPServer/Server/src/server/Sender.java
@@ -24,12 +24,6 @@ public class Sender implements Runnable {
 	@Override
 	public void run() {
 
-		ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Main.executorService;
-		int poolSize = threadPoolExecutor.getPoolSize();// 스레드 풀 사이즈 얻기
-		String threadName = Thread.currentThread().getName();// 스레드 풀에 있는 해당 스레드 이름 얻기
-
-		System.out.println("Sender [총 스레드 개수:" + poolSize + "] 작업 스레드 이름: " + threadName);
-
 		System.out.println("Send Task (IO,x,y,qty): " + msg.getTask().getIo() + "," + msg.getTask().getLocX() + ","
 				+ msg.getTask().getLocY() + "," + msg.getTask().getQty());
 
