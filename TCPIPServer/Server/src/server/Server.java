9ac5bf38a30ff8108dc0539707b920950edd4299
Yeojin Choi <who3637@naver.com>
2020-05-11 01:39:09 +0900

    Update Server.java

--- TCPIPServer/Server/src/server/Server.java
+++ TCPIPServer/Server/src/server/Server.java
@@ -35,12 +35,6 @@ public class Server implements Runnable {
 				socket = serverSocket.accept();
 				System.out.println("new Client Accepted : "+socket.getInetAddress());
 				
-				ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Main.executorService;
-				int poolSize = threadPoolExecutor.getPoolSize();//스레드 풀 사이즈 얻기
-				String threadName = Thread.currentThread().getName();//스레드 풀에 있는 해당 스레드 이름 얻기
-	            
-	            System.out.println("Server [총 스레드 개수:" + poolSize + "] 작업 스레드 이름: "+threadName);
-				
 				Runnable r = new Receiver(socket);
 				Main.executorService.execute(r);
 				
